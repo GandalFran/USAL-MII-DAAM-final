55cb4583d42ee0dfac68c21d802ebbaffac2c571
Francisco Pinto-Santos <franpintosantos@usal.es>
2021-06-01 18:10:09 +0200

    .

--- Mascotas/app/src/main/java/com/daam/mascotas/service/PetService.java
+++ Mascotas/app/src/main/java/com/daam/mascotas/service/PetService.java
@@ -1,18 +1,21 @@
 package com.daam.mascotas.service;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Parcelable;
 import android.util.Log;
-import android.view.View;
 
 import androidx.annotation.Nullable;
 
 import com.daam.mascotas.Constants;
-import com.daam.mascotas.MainActivity;
+import com.daam.mascotas.R;
 import com.daam.mascotas.bean.Pet;
-import com.daam.mascotas.bean.PetAdapter;
-import com.daam.mascotas.model.PetModel;
 import com.daam.mascotas.model.loader.JSONPetLoaderImpl;
 import com.daam.mascotas.model.loader.PetLoader;
 import com.daam.mascotas.model.loader.TextPetLoaderImpl;
@@ -23,14 +26,18 @@ import java.util.List;
 
 public class PetService extends Service {
 
-    private PetModel model;
-    private List<Pet> petsToRegister;
+    private static final int PET_NOTIFICATION_ID = 1;
+    private static final String PET_KEY = "PET_KEY";
+    private final String NOTIFICATION_CHANNEL_ID = "com.daam.mascotas.petservice";
+
+    private List<Pet> receivedPets;
+    private NotificationManager manager;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        this.model = PetModel.build(null);
-        this.petsToRegister = new ArrayList<>();
+        this.buildNotificationChannel();
+        this.receivedPets = new ArrayList<>();
     }
 
     @Override
@@ -49,8 +56,8 @@ public class PetService extends Service {
 
                     // retrieve p
                     Pet p = PetService.this.checkForPets(Constants.CENTINEL_URL);
-                    if(p!=null && !PetService.this.model.exists(p) && !PetService.this.exists(p)){
-                        PetService.this.petsToRegister.add(p);
+                    if(p!=null && !PetService.this.exists(p)){
+                        PetService.this.receivedPets.add(p);
                         PetService.this.notifyPet(p);
                     }
                 }
@@ -92,12 +99,33 @@ public class PetService extends Service {
         return p;
     }
 
+    private void buildNotificationChannel() {
+        this.manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String name = getString(R.string.notification_title);
+        String description = getString(R.string.notification_description);
+        NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, NotificationManager.IMPORTANCE_HIGH);
+        channel.setDescription(description);
+        channel.enableLights(true);
+        channel.enableVibration(true);
+        this.manager.createNotificationChannel(channel);
+    }
+
     private void notifyPet(Pet p){
         Log.d("ASDF", "Nueva mascota: " + p.toString());
+        Intent intent = new Intent(this, PetServiceReceiver.class);
+        intent.putExtra(PET_KEY, (Parcelable) p);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification.Builder builder = new Notification.Builder(
+                getApplicationContext(), NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.stat_sys_warning)
+                .setAutoCancel(true)
+                .setContentTitle("Notificación desde la IU")
+                .setContentIntent(pendingIntent);
+        this.manager.notify(PET_NOTIFICATION_ID, builder.build());
     }
 
     private boolean exists(Pet p) {
-        for(Pet pet: this.petsToRegister){
+        for(Pet pet: this.receivedPets){
             if(pet.getId().equals(p.getId())){
                 return true;
             }
